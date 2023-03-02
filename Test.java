48d3df6d288a1291db14c60343ea07cfbb66775f
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-03-02 22:46:30 +0500

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Test {
+    // // public static void main(String[] args) {
+    // //     int ar[]= {12,23,45,67};
+    // //     System.out.println("array:"+Arrays.toString(ar));
+    // // }
+    // // public static void main(String[] args) {
+    //     // int ar [] = {11,20,33,15,12};
+    //     // System.out.println("Elements of array are : " ;
+
+    //     public static void main(String[] args) {
+    //         int ar [] = new int[5];
+    //         ar[0]=20;
+    //         ar[3]=300;
+    //         ar[1]=140;
+    //         System.out.println("Elements of array are : " + Arrays.toString(ar));
+    //         }
+
+    public static void main(String[] args) {  
+        int Arr[] = {18, 30, 15, 70, 12};  
+     int i;  
+        System.out.println("Elements of the array are:"); 
+        for(i = 0; i<Arr.length; i++) {  
+           System.out.printf("Arr[%d] = %d,  ", i, Arr[i]);  
+        }  
+     }  
+        }
+    
+
