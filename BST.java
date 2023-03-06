c2eb0d5a1650056c91b2581c44ddacafccb149ed
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-03-06 09:40:55 +0500

    Add files via upload

--- BST.java
+++ BST.java
@@ -88,6 +88,51 @@ TLNode delete(TLNode root, int data)
         } 
         return minv; 
     } 
+	// insert data in tree 
+
+	void insert(int data) {
+		struct node *tempNode = (struct node*) malloc(sizeof(struct node));
+		struct node *current;
+		struct node *parent;
+	 
+		tempNode->data = data;
+		tempNode->leftChild = NULL;
+		tempNode->rightChild = NULL;
+	 
+		//if tree is empty, create root node
+		if(root == NULL) {
+		   root = tempNode;
+		} else {
+		   current = root;
+		   parent  = NULL;
+	 
+		   while(1) {                
+			  parent = current;
+	 
+			  //go to left of the tree
+			  if(data < parent->data) {
+				 current = current->leftChild;                
+				 
+				 //insert to the left
+				 if(current == NULL) {
+					parent->leftChild = tempNode;
+					return;
+				 }
+			  }
+				 
+			  //go to right of the tree
+			  else {
+				 current = current->rightChild;
+				 
+				 //insert to the right
+				 if(current == NULL) {
+					parent->rightChild = tempNode;
+					return;
+				 }
+			  }
+		   }            
+		}
+	 }
 
    
    public static void main(String[] args)
